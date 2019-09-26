5d06bdfa7517040f800fd6ac93b3f0b43c2a3ff5
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:49:55 +0700

    Add files via upload

---
+++ KambingStatic.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan6_KambingStatic;
+
+/**
+ *
+ * @author 
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class KambingStatic {
+    //NAMA_KAMBING sebagai konstanta
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main (String[] args) {
+        PBOUlang_10116584_Latihan6_KambingStatic.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING +" Memiliki Kambing Sebanyak " + PBOUlang_10116584_Latihan6_KambingStatic.jumlahKambing);
+    }
+}
---
+++ PBOUlang_10116584_Latihan6_KambingStatic.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan6_KambingStatic;
+
+/**
+ *
+ * @author
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan6_KambingStatic {
+   // Variabel Jumlah Kambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+    }
+    
